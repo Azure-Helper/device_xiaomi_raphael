4db63de12d9efa737fa50f256646db66e04a3adb
AzureHelper <azurehelper219@gmail.com>
2022-03-26 19:50:19 +0530

    raphael: Initial build properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,9 +91,12 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
-
-# QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
+
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
