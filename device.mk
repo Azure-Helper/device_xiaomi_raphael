758d894ef105fe56f94990910d11db3d8bf6bee1
Azure-Helper <azurehelper219@gmail.com>
2022-05-03 20:51:18 +0530

    raphael: Import WifiResTarget from stock

--- device.mk
+++ device.mk
@@ -449,7 +449,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
-    WifiOverlay \
+    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
